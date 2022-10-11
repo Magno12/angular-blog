bb2d32b1da5e04dae458d1a2e12ba7c790582fd9
Magno batista <magnobatista75@gmail.com>
2022-10-11 15:42:38 -0300

    content dinamico

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,20 +1,23 @@
 export const dataFake = [
     {
-        "id": 1,
+        "id": "1",
         "photo": "",
         "title": "Saiu novo filme",
-        "description": "Ola"
+        "description": "Ola",
+        "photoCover":"https://i.ytimg.com/vi/XFRS36DsToE/maxresdefault.jpg"
     },
     {
-        "id": 2,
+        "id": "2",
         "photo": "",
         "title": "Saiu novo filme",
-        "description": "Ola"
+        "description": "Ola",
+        "photoCover":"https://img1.ak.crunchyroll.com/i/spire4/1c1df98707aa0f22aa54342af725e48a1491245343_main.jpg"
     },
     {
-        "id": 3,
+        "id": "3",
         "photo": "",
         "title": "Saiu novo filme",
-        "description": "Ola"
+        "description": "Ola",
+        "photoCover":"https://criticalhits.com.br/wp-content/uploads/2019/03/naruto-shippuden-blood-prision-01.jpg"
     }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -18,12 +19,24 @@ export class ContentComponent implements OnInit {
   @Input()
   contentDescription: string = "";
 
+  private id: string | null = "0";
+
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(
-      value => console.log(value.get("id"))
-    )
+    this.route.paramMap.subscribe(value =>
+      this.id = value.get("id"))
+
+    this.setValuesComponent(this.id);
+  }
+
+  setValuesComponent(id: string | null) {
+    const result = dataFake.filter(article =>
+      article.id == id)[0]
+
+    this.contentDescription = result.description;
+    this.contentTitle = result.title;
+    this.photoCover = result.photoCover;
   }
 
 }
